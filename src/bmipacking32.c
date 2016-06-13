b1d98653a31ccdb466804ab3e0c37bc090f2cd76
Daniel Lemire <lemire@gmail.com>
2016-06-13 12:09:50 -0400

    Slightly more portable.

--- src/bmipacking32.c
+++ src/bmipacking32.c
@@ -2129,7 +2129,25 @@ static void unpackblock0(const uint8_t ** pw, uint32_t ** pout) {
   *pout += 32; /* we wrote 32 32-bit integers */
 }
 
-#include <x86intrin.h>
+#if defined(_MSC_VER)
+     /* Microsoft C/C++-compatible compiler */
+     #include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+     /* GCC-compatible compiler, targeting x86/x86-64 */
+     #include <x86intrin.h>
+#elif defined(__GNUC__) && defined(__ARM_NEON__)
+     /* GCC-compatible compiler, targeting ARM with NEON */
+     #include <arm_neon.h>
+#elif defined(__GNUC__) && defined(__IWMMXT__)
+     /* GCC-compatible compiler, targeting ARM with WMMX */
+     #include <mmintrin.h>
+#elif (defined(__GNUC__) || defined(__xlC__)) && (defined(__VEC__) || defined(__ALTIVEC__))
+     /* XLC or GCC-compatible compiler, targeting PowerPC with VMX/VSX */
+     #include <altivec.h>
+#elif defined(__GNUC__) && defined(__SPE__)
+     /* GCC-compatible compiler, targeting PowerPC with SPE */
+     #include <spe.h>
+#endif
 
 
 
@@ -3806,8 +3824,3 @@ void bmiunpack32(const uint8_t * in, uint32_t number, const uint32_t bit, uint32
 	  bitunpack(&in,&out);
   }
 }
-
-
-
-
-
